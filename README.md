c16d45023ba39df5b1d3e071a071c41f7ef12580
kardasbart <kardasbart@gmail.com>
2025-04-03 13:53:48 +0200

    Lexem generator

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wordle-solver
 Simple program for solving wordle
+
+
+## Morfeusz
+
+Do odfiltrowania wyrazów we wszystkich odmianach:
+
+http://morfeusz.sgjp.pl/
+
+pip install http://download.sgjp.pl/morfeusz/20250330/Linux/manylinux_2_28/64/morfeusz2-1.99.10-cp36-abi3-manylinux_2_28_x86_64.whl
---
+++ lexem_gen.py
@@ -0,0 +1,29 @@
+import morfeusz2 as mf
+import sys
+
+morf = mf.Morfeusz()
+
+def main():
+    
+    output_dictionary = set()
+    num_lines = sum(1 for _ in open(sys.argv[1]))
+    
+    with open(sys.argv[1], 'r') as file:
+        # Read each line in the file
+        for idx, line in enumerate(file):
+            print(f"{idx+1} / {num_lines}", end="\r")
+            # Print each line
+            word = line.strip()
+            ret = morf.analyse(word)
+            for r in ret:
+                lexem = r[2][1].split(":")[0]
+                output_dictionary.add(lexem)
+    
+    print(num_lines, " -> ", len(output_dictionary))
+
+    with open(sys.argv[2], 'w') as file:
+        for word in sorted(list(output_dictionary)):
+            file.write(word+"\n")
+        
+if __name__ == "__main__":
+    main()
